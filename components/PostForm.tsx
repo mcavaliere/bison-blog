0ff4522682f06abbf789fe90fa9a67974808e67e
Mike Cavaliere <mcavaliere@echobind.com>
2020-10-01 15:53:58 -0400

    Moves post form into component

--- components/PostForm.tsx
+++ components/PostForm.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
-import { Heading, Center } from '@chakra-ui/core';
+import { FormLabel, FormControl, FormErrorMessage, Input, Button, Textarea } from '@chakra-ui/core';
+import { useForm } from 'react-hook-form';
 
 /** Description of component */
 export function PostForm() {
-  const message = 'Hola! PostForm Created!';
+  const { register, handleSubmit, errors } = useForm();
+  const onSubmit = (data) => console.warn(data);
 
   return (
-    <Center bg="red.300" color="white" padding={{ base: 16, lg: 24 }}>
-      <Heading>{message}</Heading>
-    </Center>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <FormControl isInvalid={errors.title}>
+        <FormLabel htmlFor="title">Post Title</FormLabel>
+        <Input type="text" name="title" id="title" ref={register({ required: true })} />
+        <FormErrorMessage>{errors.title && <span>This field is required</span>}</FormErrorMessage>
+      </FormControl>
+
+      <FormControl isInvalid={errors.body}>
+        <FormLabel htmlFor="boty">Post Body</FormLabel>
+        <Textarea name="body" id="body" ref={register({ required: true })} />
+        <FormErrorMessage>{errors.body && <span>This field is required</span>}</FormErrorMessage>
+      </FormControl>
+
+      <Button type="submit">Submit</Button>
+    </form>
   );
-};
+}
--- pages/posts/new.tsx
+++ pages/posts/new.tsx
@@ -1,23 +1,10 @@
 import React from 'react';
 import Head from 'next/head';
-import {
-  Heading,
-  Flex,
-  FormLabel,
-  FormControl,
-  FormErrorMessage,
-  Input,
-  Button,
-  Textarea,
-} from '@chakra-ui/core';
-import { useForm } from 'react-hook-form';
+import { Heading, Flex } from '@chakra-ui/core';
+
+import { PostForm } from '../../components/PostForm';
 
 function PostsNewPage() {
-  const { register, handleSubmit, watch, errors } = useForm();
-  const onSubmit = (data) => console.warn(data);
-
-  console.log(`---------------- errors: `, errors);
-
   return (
     <>
       <Head>
@@ -27,25 +14,7 @@ function PostsNewPage() {
       <Flex direction="column" justify="center">
         <Heading size="lg">New Post</Heading>
 
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <FormControl isInvalid={errors.title}>
-            <FormLabel htmlFor="title">Post Title</FormLabel>
-            <Input type="text" name="title" id="title" ref={register({ required: true })} />
-            <FormErrorMessage>
-              {errors.title && <span>This field is required</span>}
-            </FormErrorMessage>
-          </FormControl>
-
-          <FormControl isInvalid={errors.body}>
-            <FormLabel htmlFor="boty">Post Body</FormLabel>
-            <Textarea name="body" id="body" ref={register({ required: true })} />
-            <FormErrorMessage>
-              {errors.body && <span>This field is required</span>}
-            </FormErrorMessage>
-          </FormControl>
-
-          <Button type="submit">Submit</Button>
-        </form>
+        <PostForm />
       </Flex>
     </>
   );
